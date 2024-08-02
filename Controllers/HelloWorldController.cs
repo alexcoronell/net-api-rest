cde7e517d7f459f4c73dea11f29a6398fbb63b03
Alex Coronell <alexcoronell@gmail.com>
2024-08-02 15:42:02 -0500

    Swagger fixed, tests with ILogger

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,13 +8,18 @@ public class HelloWorldController : ControllerBase
 {
     IHelloWorldService helloWorldServices;
 
-    public HelloWorldController(IHelloWorldService helloWorld)
+    private readonly ILogger<HelloWorldController> _logger;
+
+    public HelloWorldController(IHelloWorldService helloWorld, ILogger<HelloWorldController> logger)
     {
+        _logger = logger;
         helloWorldServices = helloWorld;
     }
 
+    [HttpGet]
     public IActionResult Get()
     {
+        _logger.LogInformation("return Hello World");
         return Ok(helloWorldServices.GetHelloWorld());
     }
 }
